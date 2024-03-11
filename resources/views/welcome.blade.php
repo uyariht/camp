7585d5b8bc97dd15ce5f2f424dbacf8df5a18c01
Copter0v0 <88174531+Copter0v0@users.noreply.github.com>
2024-03-11 18:24:07 +0700

    p

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -831,6 +831,7 @@
 </head>
 
 <body class="antialiased">
+    <h1>Welcome to cluster 4</h1>
     <div
         class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
         @if (Route::has('login'))
@@ -880,7 +881,6 @@
             </div>
         </div>
     </div>
-    <h1>Welcome to cluster 4</h1>
 </body>
 
 </html>
